1794a4a9f88eb057d1e43e99bf50d15b4138cc2b
Ray Dudu <raydudu@gmail.com>
2022-06-19 23:10:30 +0200

    Initial port to STM32H735G-DK

---
+++ Core/Src/i2c.c
@@ -0,0 +1,130 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    i2c.c
+  * @brief   This file provides code for the configuration
+  *          of the I2C instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "i2c.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+I2C_HandleTypeDef hi2c4;
+
+/* I2C4 init function */
+void MX_I2C4_Init(void)
+{
+
+  /* USER CODE BEGIN I2C4_Init 0 */
+
+  /* USER CODE END I2C4_Init 0 */
+
+  /* USER CODE BEGIN I2C4_Init 1 */
+
+  /* USER CODE END I2C4_Init 1 */
+  hi2c4.Instance = I2C4;
+  hi2c4.Init.Timing = 0x0000020B;
+  hi2c4.Init.OwnAddress1 = 0;
+  hi2c4.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c4.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c4.Init.OwnAddress2 = 0;
+  hi2c4.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c4.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c4.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c4, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c4, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C4_Init 2 */
+
+  /* USER CODE END I2C4_Init 2 */
+
+}
+
+void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(i2cHandle->Instance==I2C4)
+  {
+  /* USER CODE BEGIN I2C4_MspInit 0 */
+
+  /* USER CODE END I2C4_MspInit 0 */
+
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    /**I2C4 GPIO Configuration
+    PF15     ------> I2C4_SDA
+    PF14     ------> I2C4_SCL
+    */
+    GPIO_InitStruct.Pin = I2C4_SDA_Pin|I2C4_SCL_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C4;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    /* I2C4 clock enable */
+    __HAL_RCC_I2C4_CLK_ENABLE();
+  /* USER CODE BEGIN I2C4_MspInit 1 */
+
+  /* USER CODE END I2C4_MspInit 1 */
+  }
+}
+
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
+{
+
+  if(i2cHandle->Instance==I2C4)
+  {
+  /* USER CODE BEGIN I2C4_MspDeInit 0 */
+
+  /* USER CODE END I2C4_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C4_CLK_DISABLE();
+
+    /**I2C4 GPIO Configuration
+    PF15     ------> I2C4_SDA
+    PF14     ------> I2C4_SCL
+    */
+    HAL_GPIO_DeInit(I2C4_SDA_GPIO_Port, I2C4_SDA_Pin);
+
+    HAL_GPIO_DeInit(I2C4_SCL_GPIO_Port, I2C4_SCL_Pin);
+
+  /* USER CODE BEGIN I2C4_MspDeInit 1 */
+
+  /* USER CODE END I2C4_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# audio_pass-through<br>
-Audio pass-through STM32F769I-Discovery Line in buffer copied to Line out<br>
-This software builds heavily on the great work by https://community.st.com/people/Beaulier.Francois<br>
-wm8994.c is modified from the BSP version<br>
-Input Mixer L and R zeroed and muted registers 0x29 and 0x2A<br>
-Read the data sheet. I know its long and complicated. Do it anyway.<br>
-SW4STM32 project using STM32 HAL<br>
-Output on serial port looks like<br>
-Connected to STM32F769I-Discovery USART 1<br>
-<br>
-Audio I/O initialization OK<br>
-SAI receive begin OK<br>
-Copying Record buffer to Playback buffer<br>
-Audio output OK<br>
-<br>
-Line level input is expected, not speaker level so turn your volume down at least to 50% or lower<br>
-<br>
-I have not tested microphone input or speaker output<br>
+# Audio Loopback excample
+
+Audio loopback STM32F769I-Discovery Line in buffer copied to Line out.
+This software builds heavily on the great work by https://community.st.com/people/Beaulier.Francois
+
+wm8994.c is modified from the BSP version  
+Input Mixer L and R zeroed and muted registers 0x29 and 0x2A  
+Read the data sheet. I know its long and complicated. Do it anyway.  
+SW4STM32 project using STM32 HAL  
+Output on serial port looks like  
+Connected to STM32F769I-Discovery USART 1  
+
+Line level input is expected, not speaker level so turn your volume down at least to 50% or lower
